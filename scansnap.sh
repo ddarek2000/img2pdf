eabbaeb0e98dad9f3d55fe73e248907db4c9fa11
ddarek2000 <40216289+ddarek2000@users.noreply.github.com>
2019-06-05 16:06:17 -0400

    Update scansnap.sh

--- scansnap.sh
+++ scansnap.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-#scanimage --resolution 600 --batch="scan_%03d.pnm"> /mnt/temp/SCANNER/foo.pnm
-#scanimage --resolution 600 --batch="/mnt/temp/SCANNER/scan_%03d.pnm" --batch-double --format=pnm
-#exit 0
+#set -e
 
-set -e
 export TMP_DIR=/mnt/temp/SCANNER/scan_$(date "+%Y-%m-%d_%H%M%S")
 mkdir -p $TMP_DIR
 
@@ -11,25 +8,34 @@ mkdir -p $TMP_DIR
 #scanimage --all-options >> /mnt/temp/SCANNER/scanimage.log
 
 echo 'scanning..'
-scanimage --source "ADF Duplex" --resolution 300 --batch="$TMP_DIR/scan_%03d.tiff" --format=tiff --mode Gray --page-width 215.9 --page-
-height 355.6 -y 355.6 -x 215.9 --swcrop=yes --swskip=1 --buffermode on --sleeptimer 5 --offtimer 30 --ald=yes
-#scanimage --source "ADF Duplex" --resolution 300 --batch="$TMP_DIR/scan_%03d.tiff" --format=tiff --mode Gray --page-width 215.9 --page
--height 355.6 -y 355.6 -x 215.9 --swcrop=yes --swskip=1 --buffermode on
-#scanimage --source "ADF Duplex" --resolution 300 --batch="$TMP_DIR/scan_%03d.tiff" --format=tiff --mode Gray --page-width 215.9 --page
--height 355.6 -y 355.6 -x 215.9 --swcrop=yes --swskip=1
-#scanimage --source "ADF Duplex" --resolution 600 --batch="$TMP_DIR/scan_%03d.tiff" --format=tiff --mode Gray --page-width 215.9 --page
--height 355.6 -y 355.6 -x 215.9 --swcrop=yes --swskip=1
-#scanimage --source "ADF Duplex" --resolution 600 --batch="$TMP_DIR/scan_%03d.tiff" --format=tiff --mode Gray --page-width 215.9 --page
--height 355.6 -y 355.6 -x 215.9 --swcrop=yes
-#scanimage --source "ADF Duplex" --resolution 600 --batch="$TMP_DIR/scan_%03d.tiff" --format=tiff --mode Gray --page-width 215.9 --page
--height 355.6 -y 355.6 -x 215.9 --buffermode On
+scanimage --source "ADF Duplex" --resolution 300 --batch="$TMP_DIR/scan_%03d.pnm" --format=pnm --mode Gray --page-width 215.9 --page-height 355.6 -y 355.6 -x 215.9 --swcrop=yes --swskip=1 --buffermode on --sleeptimer 5 --offtimer 30 --ald=yes
+
+############################
+# OLD
+############################
+#scanimage --source "ADF Duplex" --resolution 300 --batch="$TMP_DIR/scan_%03d.tiff" --format=tiff --mode Gray --page-width 215.9 --page-height 355.6 -y 355.6 -x 215.9 --swcrop=yes --swskip=1 --buffermode on --sleeptimer 5 --offtimer 30 --ald=yes
+#scanimage --source "ADF Duplex" --resolution 300 --batch="$TMP_DIR/scan_%03d.tiff" --format=tiff --mode Gray --page-width 215.9 --page -height 355.6 -y 355.6 -x 215.9 --swcrop=yes --swskip=1 --buffermode on
+#scanimage --source "ADF Duplex" --resolution 300 --batch="$TMP_DIR/scan_%03d.tiff" --format=tiff --mode Gray --page-width 215.9 --page -height 355.6 -y 355.6 -x 215.9 --swcrop=yes --swskip=1
+#scanimage --source "ADF Duplex" --resolution 600 --batch="$TMP_DIR/scan_%03d.tiff" --format=tiff --mode Gray --page-width 215.9 --page -height 355.6 -y 355.6 -x 215.9 --swcrop=yes --swskip=1
+#scanimage --source "ADF Duplex" --resolution 600 --batch="$TMP_DIR/scan_%03d.tiff" --format=tiff --mode Gray --page-width 215.9 --page -height 355.6 -y 355.6 -x 215.9 --swcrop=yes
+#scanimage --source "ADF Duplex" --resolution 600 --batch="$TMP_DIR/scan_%03d.tiff" --format=tiff --mode Gray --page-width 215.9 --page -height 355.6 -y 355.6 -x 215.9 --buffermode On
 #scanimage --source "ADF Duplex" --resolution 600 --batch="$TMP_DIR/scan_%03d.tiff" --format=tiff --mode Gray -y 558.728
 #scanimage --resolution 600 --batch="$TMP_DIR/scan_%03d.tiff" --format=tiff --mode Gray original-size=Auto --source "ADF Duplex"
 #scanimage --resolution 600 --batch="$TMP_DIR/scan_%03d.tiff" --format=tiff --mode Gray --source "ADF Duplex"
 #scanimage --resolution 600 --batch="$TMP_DIR/scan_%03d.pnm" --format=pnm --mode Gray --source "ADF Duplex"
+#scanimage --resolution 600 --batch="scan_%03d.pnm"> /mnt/temp/SCANNER/foo.pnm
+#scanimage --resolution 600 --batch="/mnt/temp/SCANNER/scan_%03d.pnm" --batch-double --format=pnm
 
+echo 'check if anything got scanned'
+if [ `find $TMP_DIR -name "*.pnm" | wc -l` = 0 ]; then
+        rmdir $TMP_DIR
+        # try to import pictures from camera
+        /etc/scanbd/scripts/pictures.sh > /dev/null 2>&1
+        exit 0
+fi
 echo 'packaging and uploading in subshell'
-find $TMP_DIR -name "*.tiff" -exec basename {} \; | sort > $TMP_DIR/scanimage.txt
+find $TMP_DIR -name "*.pnm" -exec basename {} \; | sort > $TMP_DIR/scanimage.txt
+
 
 #echo 'convert to pdf'
 #/usr/bin/img2pdf -o $TMP_DIR/out.pdf `cat $TMP_DIR/scanimage.txt`
