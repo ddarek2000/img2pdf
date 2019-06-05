bfc46910fe38734d03b1f3147cab3b390fc6a131
ddarek2000 <40216289+ddarek2000@users.noreply.github.com>
2019-06-05 16:03:55 -0400

    Update scanner_wacher.sh

--- scanner_wacher.sh
+++ scanner_wacher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#set -x
+set -x
 
 SCANNER_BASE=/mnt/cache/temp/SCANNER
 SCANNER_NEXTCLOUD=/mnt/cache/nextcloud/data/DOCUMENTS/files/SCANNER
@@ -41,12 +41,14 @@ fi
 ## find files
 ###################################################################################
 for i in $( find $SCANNER_BASE -name scanimage.txt -type f -mmin -60 );do
+#for i in $( find $SCANNER_BASE -name scanimage.txt -type f );do
 ###################################################################################
 
 SCAN_DIR=$(basename $(dirname "$i"))
 if [ ! -e "$SCANNER_BASE/$SCAN_DIR/pdf.lock" ]; then
         echo "Lock File does not exist"
         touch $SCANNER_BASE/$SCAN_DIR/pdf.lock
+        chown -R 99:98 $SCANNER_BASE/$SCAN_DIR
 else
     exit 0
 fi
@@ -55,8 +57,8 @@ fi
 ### postprocess the images -- done in hardware now
 for j in $(find $SCANNER_BASE/$SCAN_DIR \( -name "*.tiff" -o -name "*.pnm" \) -printf "%f\n");do
 
-/usr/bin/docker run --rm -v "$SCANNER_BASE:/home/docker" img2pdf magick convert -flatten -fuzz 5% -fill "#ffffff" -opaque "#f2f2f2" -tr
-im +repage -density 300 -depth 8 -strip -background white -alpha off $SCAN_DIR/$j $SCAN_DIR/$j
+/usr/bin/docker run --rm -v "$SCANNER_BASE:/home/docker" ddarek/img2pdf magick convert -flatten -fuzz 5% -fill "#ffffff" -opaque "#f2f2f2" -trim +repage -density 300 -depth 8 -st
+rip -background white -alpha off $SCAN_DIR/$j $SCAN_DIR/$j
 
 done
 ret_code=$?
@@ -66,8 +68,7 @@ if [ $ret_code -ne 0 ]; then rm $SCANNER_BASE/$SCAN_DIR/pdf*.pdf ; exit 1; fi
 ### remove blank pages
 #for j in $( find $SCANNER_BASE/$SCAN_DIR \( -name "*.tiff" -o -name "*.pnm" \) -type f -printf "%f\n" );do
 
-#       is_blank=$(/usr/bin/docker run --rm -v "$SCANNER_BASE:/home/docker" img2pdf magick convert "$SCAN_DIR/$j" -format "%[fx:mean>0.
-99?1:0]" info:)
+#       is_blank=$(/usr/bin/docker run --rm -v "$SCANNER_BASE:/home/docker" ddarek/img2pdf magick convert "$SCAN_DIR/$j" -format "%[fx:mean>0.99?1:0]" info:)
 #       if [ $is_blank -ne 0 ];then
 #               echo "$SCAN_DIR/$j is blank"
 #               rm $SCANNER_BASE/$SCAN_DIR/$j
@@ -80,19 +81,17 @@ if [ $ret_code -ne 0 ]; then rm $SCANNER_BASE/$SCAN_DIR/pdf*.pdf ; exit 1; fi
 ###################################################################################
 
 ### non blank list
-find $SCANNER_BASE/$SCAN_DIR \( -name "*.tiff" -o -name "*.pnm" \) -printf "$SCAN_DIR/%f\n"| sort > $SCANNER_BASE/$SCAN_DIR/pdf_nonblan
-k.txt
+find $SCANNER_BASE/$SCAN_DIR \( -name "*.tiff" -o -name "*.pnm" \) -printf "$SCAN_DIR/%f\n"| sort > $SCANNER_BASE/$SCAN_DIR/pdf_nonblank.txt
 ret_code=$?
 if [ $ret_code -ne 0 ]; then rm $SCANNER_BASE/$SCAN_DIR/pdf*.pdf ; exit 1; fi
 
 ### convert to a single pdf
-docker run --rm -v "$SCANNER_BASE:/home/docker" img2pdf img2pdf -o $SCAN_DIR/pdf.pdf `cat $SCANNER_BASE/$SCAN_DIR/pdf_nonblank.txt`
+docker run --rm -v "$SCANNER_BASE:/home/docker" ddarek/img2pdf img2pdf -o $SCAN_DIR/pdf.pdf `cat $SCANNER_BASE/$SCAN_DIR/pdf_nonblank.txt`
 ret_code=$?
 if [ $ret_code -ne 0 ]; then rm $SCANNER_BASE/$SCAN_DIR/pdf*.pdf ; exit 1; fi
 
 ### ocr the pdf file
-docker run --rm -v "$SCANNER_BASE:/home/docker" img2pdf ocrmypdf --deskew --rotate-pages --rotate-pages-threshold 3 $SCAN_DIR/pdf.pdf $
-SCAN_DIR/pdf_output.pdf
+docker run --rm -v "$SCANNER_BASE:/home/docker" ddarek/img2pdf ocrmypdf --deskew --rotate-pages --rotate-pages-threshold 3 $SCAN_DIR/pdf.pdf $SCAN_DIR/pdf_output.pdf
 ret_code=$?
 if [ $ret_code -ne 0 ]; then rm $SCANNER_BASE/$SCAN_DIR/pdf*.pdf ; exit 1; fi
 
@@ -120,8 +119,8 @@ chmod -R 777 $SCANNER_NEXTCLOUD
 ### update file index and text search index
 /usr/bin/docker exec nextcloud sudo -u abc php /config/www/nextcloud/occ files:scan --path="DOCUMENTS/files/SCANNER" > /dev/null 2>&1
 /usr/bin/docker exec nextcloud sudo -u abc php /config/www/nextcloud/occ fulltextsearch:stop > /dev/null 2>&1
-/usr/bin/docker exec nextcloud sudo -u abc php -d memory_limit=4096M /config/www/nextcloud/occ fulltextsearch:index "{\"user\":\"DOCUME
-NTS\", \"providers\":\"files\", \"path\":\"SCANNER\"}"  > /dev/null 2>&1
+/usr/bin/docker exec nextcloud sudo -u abc php -d memory_limit=4096M /config/www/nextcloud/occ fulltextsearch:index "{\"user\":\"DOCUMENTS\", \"providers\":\"files\", \"path\":\"
+SCANNER\"}"  > /dev/null 2>&1
 
 done
 
@@ -130,9 +129,9 @@ done
 
 ###################################################################################
 ### Cleanup dirs older than 24 hrs
-for i in $( find $SCANNER_BASE/ -name scanimage_pdf_generated.txt -type f -mtime +1 );do
-        rm -rf $(dirname "$i")
-done
+#for i in $( find $SCANNER_BASE/ -name scanimage_pdf_generated.txt -type f -mtime +1 );do
+#       rm -rf $(dirname "$i")
+#done
 
 #cleanup lock file
 rm /tmp/scanner_watcher.pid
